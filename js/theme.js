1fcfc3f26c0979ca2dbcd95e163e86aab1ac2297
rk2348 <ryoma.k0718@icloud.com>
2026-01-10 03:53:15 +0900

    アニメーションの変更

--- js/theme.js
+++ js/theme.js
@@ -3,9 +3,9 @@ const members = [
     { name: "鎮西寿々歌", color: "#ff9f43" },
     { name: "早瀬ノエル", color: "#feca57" },
     { name: "櫻井優衣", color: "#55efc4" },
-    { name: "仲川瑠夏", color: "#74b9ff" },
+    { name: "真中まな", color: "#74b9ff" },
     { name: "松本かれん", color: "#ff9ff3" },
-    { name: "真中まな", color: "#e2e2e2" }
+    { name: "仲川瑠夏", color: "#9457EB" }
 ];
 
 function initTheme() {
